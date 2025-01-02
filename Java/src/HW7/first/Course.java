fcc30bbbc8f6d60a887ed721914b56ca5fc5382e
Anton Oparin <gameslemon79@gmail.com>
2025-01-02 23:03:01 +0100

    test

---
+++ Java/src/HW7/first/Course.java
@@ -0,0 +1,36 @@
+package HW7.first;
+
+public class Course{
+    String name;
+    String tutor;
+    int studentCount;
+    public Course(String name,String tutor){
+        this.name = name;
+        this.tutor = tutor;
+        this.studentCount = 0;
+    }
+    public void incrementStudentCount() {
+        studentCount++;
+    }
+    public void decrementStudentCount() {
+        if (studentCount > 0) {
+            studentCount--;
+        }
+        else{
+            System.out.println("There is no students in this course");
+        }
+
+    }
+    public String getName() {
+        return name;
+    }
+
+
+
+    public void displayInfo() {
+        System.out.println("Курс: " + name);
+        System.out.println("Викладач: " + tutor);
+        System.out.println("Кількість студентів: " + studentCount);
+    }
+}
+
---
+++ Java/src/HW7/second/Balance.java
@@ -0,0 +1,38 @@
+package HW7.second;
+
+public class Balance {
+    int balanceId;
+    double balance;
+//Конструктор для класу Рахунок, який приймає номер рахунку та початковий баланс.
+    public Balance(int balanceId,double initialBalance){
+        this.balanceId = balanceId;
+        this.balance = initialBalance;
+
+    }
+//Метод покластиГроші() в класі Рахунок, який додає вказану суму на баланс рахунку.
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Додано: " + amount + ". Новий баланс: " + balance);
+    }
+    //Метод знятиГроші() в класі Рахунок, який знімає вказану суму з балансу рахунку. Перевірити, чи достатньо коштів на рахунку.
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Недостатньо коштів для зняття: " + amount);
+        } else {
+            balance -= amount;
+            System.out.println("Знято: " + amount + ". Новий баланс: " + balance);
+        }
+    }
+    public int getBalanceId() {
+        return balanceId;
+    }
+
+
+    //Метод вивестиІнформацію() в обох класах, який виводить інформацію про об'єкт у консоль.
+    public void getInfo(){
+        System.out.println("Номер балансу: " + balanceId);
+        System.out.println("Баланс: " + balance);
+    }
+}
+
